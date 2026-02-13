653dd3474246ec5f9728e256fbe77f8cb9e976eb
Karthik <karthikjai211@gmail.com>
2026-02-13 15:12:36 +0530

    updated

--- Patterns.cpp
+++ Patterns.cpp
@@ -79,29 +79,164 @@
 
 //4
 
+// #include <iostream>
+// using namespace std;
+
+// class Pattern{
+//     public:
+//         void squarePatterns(int n,int num ){
+//             for(int i =0; i <n;i++){
+//                 for(int j = 0; j<n;j++){
+//                     cout << num << " ";
+//                     num++;
+//                 }
+//                 cout << endl;
+//             }
+//         }
+// };
+
+// int main(){
+//     int n ;
+//     int num = 1;
+//     cout << "enter number:";
+//     cin >> n;
+
+//     Pattern kar;
+//     kar.squarePatterns(n,num);
+
+// }
+
+
+// 5  Triangle
+
+// #include <iostream>
+// using namespace std;
+
+// class Solution{
+//     public:
+//         void patterns(int n ){
+//             for(int i = 1 ; i<=n ; i++){
+//                 for(int j = 0;j<i ; j++ ){
+//                     cout<<"*";
+
+//                 }
+//                 cout << endl;
+//             }
+//         }
+// };
+
+// int main(){
+//     int n;
+//     cout << "number:";
+//     cin>> n;
+
+//     Solution kar;
+//     kar.patterns(n);
+// }
+
+
+// 6 
+
+// #include <iostream>
+// using namespace std;
+
+// class Solution{
+//     public:
+//         void patterns (int n){
+//             for(int i =1;i <= n; i++){
+//                 for(int j = 1 ; j <= i; j++){
+//                     cout << i;
+//                 }
+//                 cout << endl;
+
+//             }
+            
+//         }
+// };
+
+// int main(){
+//     int n;
+//     cin >> n;
+
+//     Solution kar;
+//     kar.patterns(n);
+// }
+
+//7
+
+// #include <iostream>
+// using namespace std;
+
+// class Solution{
+//     public:
+//         void patterns (int n){
+//             for(int i = 1 ; i <= n; i++){
+//                 for(int j = 1 ; j <= i; j++){
+//                     cout << j << ' ';
+//                 }
+//                 cout << endl;
+//             }
+
+//         }};
+
+// int main(){
+//     int n;
+//     cin >> n;
+
+//     Solution kar;
+//     kar.patterns(n);
+// }
+
+
+//8 reserve triangle
+
+
+// #include <iostream>
+// using namespace std;
+
+// class Solution{
+//     public:
+//         void patterns (int n){
+//             for(int i = 0 ; i < n; i++){
+//                 for(int j = i +1 ; j >0; j--){
+//                     cout << j << ' ';
+//                 }
+//                 cout << endl;
+//             }
+
+//         }};
+
+// int main(){
+//     int n;
+//     cin >> n;
+
+//     Solution kar;
+//     kar.patterns(n);
+// }
+
+
+// 9 
+
+
 #include <iostream>
 using namespace std;
 
-class Pattern{
+class Solution{
     public:
-        void squarePatterns(int n,int num ){
-            for(int i =0; i <n;i++){
-                for(int j = 0; j<n;j++){
-                    cout << num << " ";
-                    num++;
+        void patterns (int n){
+            for(int i = 0 ; i < n; i++){
+                for(int j = i +1 ; j >0; j--){
+                    cout << j << ' ';
                 }
                 cout << endl;
             }
-        }
-};
+
+        }};
 
 int main(){
-    int n ;
-    int num = 1;
-    cout << "enter number:";
+    int n;
     cin >> n;
 
-    Pattern kar;
-    kar.squarePatterns(n,num);
-
+    Solution kar;
+    kar.patterns(n);
 }
